a0a3b16c7dacea4c2567cb0ca637c0d78f0f4e1c
daeun <programmerdaeun@gmail.com>
2019-12-19 18:19:34 +0900

    README생성

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Introduction
+날씨정보 크롤링 (python) 레피지토리 입니다
+___
+#### 프로젝트 소개
+1. 가상 환경 
+   python3 -m venv venv
+   . venv/bin/activate
+
+2. python main.py --tg WEATHER_CRAWLING
--- main.py
+++ main.py
@@ -7,69 +7,79 @@ import ssl
 from datetime import datetime, time
 from time import sleep
 import pymysql
+import argparse
 
-def main(logger):
+# --tg WEATHER_CRAWLING
 
-    try:
-        loop = True
-        while loop == True:
+def main(args, logger):
 
-            #DB
-            conn = pymysql.connect(host=TargetConfig.DB_HOST, user=TargetConfig.DB_USER, password=TargetConfig.DB_PW, db=TargetConfig.DB_NAME, charset='utf8')
-            curs = conn.cursor()
+    for target in args.tg:
+        if target == 'WEATHER_CRAWLING':
+        try:
+            loop = True
+            while loop == True:
 
-            print("-- 크롤링 시작 " + datetime.now().strftime('%Y%m%d %H:%M:%S'))
+                #DB
+                conn = pymysql.connect(host=TargetConfig.DB_HOST, user=TargetConfig.DB_USER, password=TargetConfig.DB_PW, db=TargetConfig.DB_NAME, charset='utf8')
+                curs = conn.cursor()
 
-            url = TargetConfig.WEATHER
-            try:
-                context = ssl._create_unverified_context()
-                html = urlopen(url, context=context)
-                source = html.read()
-                html.close()
-            except HTTPError as e:
-                err = e.read()
-                errCode = e.getcode()
-                print("HTTP ERROR >>>> " + errCode)
+                print("-- 크롤링 시작 " + datetime.now().strftime('%Y%m%d %H:%M:%S'))
 
-            response = json.loads(source)
+                url = TargetConfig.WEATHER
+                try:
+                    context = ssl._create_unverified_context()
+                    html = urlopen(url, context=context)
+                    source = html.read()
+                    html.close()
+                except HTTPError as e:
+                    err = e.read()
+                    errCode = e.getcode()
+                    print("HTTP ERROR >>>> " + errCode)
 
-            weaters = response.get("wetrMap")
-            w_time = weaters[0][1]
+                response = json.loads(source)
 
-            for i in range(1,len(weaters)):
+                weaters = response.get("wetrMap")
+                w_time = weaters[0][1]
 
-                code = weaters[i][0]
-                region = weaters[i][1].get("rgnNm")
-                temperature = int(weaters[i][1].get("tmpr"))
-                txt = weaters[i][1].get("wetrTxt")
+                for i in range(1,len(weaters)):
 
-                # -- test -- 
-                print(w_time)
-                print("지역코드: ", code)
-                print("지역이름: ", region)
-                print("기온: ", temperature)
-                print("날씨: ", txt)
-                print("~~~~~")
+                    code = weaters[i][0]
+                    region = weaters[i][1].get("rgnNm")
+                    temperature = int(weaters[i][1].get("tmpr"))
+                    txt = weaters[i][1].get("wetrTxt")
 
-                sql = 'INSERT INTO weather (code, region, temperature, w_time, c_time, txt) VALUES (%s, %s, %s, %s, now(), %s) ON DUPLICATE KEY UPDATE temperature = %s, w_time = %s, c_time = now(), txt = %s'
-                data = (code, region, temperature, w_time, txt, temperature, w_time, txt)
-                curs.execute(sql, data)
-                conn.commit()
-                print("@@ 데이터 입력 @@")
-                
-            print("-- S L E E P !")
-            sleep(10)
-        
-    except Exception as ex:
-        logger.error("main error(2) " + str(ex))
-        
+                    # -- test -- 
+                    print(w_time)
+                    print("지역코드: ", code)
+                    print("지역이름: ", region)
+                    print("기온: ", temperature)
+                    print("날씨: ", txt)
+                    print("~~~~~")
+
+                    sql = 'INSERT INTO weather (code, region, temperature, w_time, c_time, txt) VALUES (%s, %s, %s, %s, now(), %s) ON DUPLICATE KEY UPDATE temperature = %s, w_time = %s, c_time = now(), txt = %s'
+                    data = (code, region, temperature, w_time, txt, temperature, w_time, txt)
+                    curs.execute(sql, data)
+                    conn.commit()
+                    print("@@ 데이터 입력 @@")
+                    
+                print("-- S L E E P !")
+                sleep(10)
+            
+        except Exception as ex:
+            logger.error("main error(2) " + str(ex))
 
 if __name__ == '__main__':
 
     logging.basicConfig(format='[%(lineno)d]%(asctime)s||%(message)s')
     logger = logging.getLogger(name="myLogger")
 
+    # parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tg', nargs='+', help='start crawling')
+    args = parser.parse_args()
+
     try:
-        main(logger)
+        main(args, logger)
     except:
         logger.error("main error(1)")
+        raise
