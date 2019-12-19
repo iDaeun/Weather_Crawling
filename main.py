6ce3d7a582730adfbe20f8699e501b4acd6efab6
daeun <programmerdaeun@gmail.com>
2019-12-19 14:34:44 +0900

    기능: loop걸기, 크롤링한 시간/날씨 시간/지역 이름/지역 기온 print

--- main.py
+++ main.py
@@ -4,43 +4,47 @@ import json
 import logging
 from config import TargetConfig
 import ssl
-
-
+from datetime import datetime, time
+from time import sleep
 
 def main():
 
     try:
-        print("-- 크롤링 시작 --")
-        url = TargetConfig.WEATHER
-        try:
-            context = ssl._create_unverified_context()
-            html = urlopen(url, context=context)
-            source = html.read()
-            html.close()
-        except HTTPError as e:
-            err = e.read()
-            code = e.getcode()
-            print("HTTP ERROR >>>> " + code)
+        loop = True
+        while loop == True:
 
-        response = json.loads(source)
+            print("-- 크롤링 시작 " + datetime.now().strftime('%Y%m%d %H:%M:%S'))
 
-        weaters = response.get("wetrMap")
-        time = weaters[0][1]
+            url = TargetConfig.WEATHER
+            try:
+                context = ssl._create_unverified_context()
+                html = urlopen(url, context=context)
+                source = html.read()
+                html.close()
+            except HTTPError as e:
+                err = e.read()
+                code = e.getcode()
+                print("HTTP ERROR >>>> " + code)
 
-        for i in range(1,len(weaters)+1):
-            print(weaters[i][1].get("rgnNm"))
-            print(int(weaters[i][1].get("tmpr")))
-    
+            response = json.loads(source)
+
+            weaters = response.get("wetrMap")
+            time = weaters[0][1]
+            print(time)
+
+            for i in range(1,len(weaters)):
+                print(weaters[i][1].get("rgnNm"))
+                print(int(weaters[i][1].get("tmpr")))
+            
+            print("S L E E P !")
+            sleep(10)
+        
     except Exception as ex:
-        logging.info("main error(2)" + str(ex))
-    
-
-
-
+        logging.error("main error(2)" + str(ex))
 
 if __name__ == '__main__':
 
     try:
         main()
     except:
-        logging.info("main error(1)")
+        logging.error("main error(1)")
